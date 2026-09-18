3a04919db735c8ed62a86d59e96a0e21b7eccde5
Ifem1 <99996625+Ifem1@users.noreply.github.com>
2026-09-18 12:02:28 +0100

    Wire canonical Flowed contract into frontend

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.FLOWED_CONFIG = Object.freeze({
   network: 'GenLayer Studionet',
   chainId: 61999,
   rpc: 'https://studio.genlayer.com/api',
-  contractAddress: '',
+  contractAddress: '0xE7aE476b544afe3A38954BBf15f7BE3A4FA4D8Ad',
 });
